b437c18f88f2f5eb94631450bed927fe3a4feefa
olyabor <olyabor800@gmail.com>
2020-07-29 17:42:29 +0300

    third commit

--- scripts/radioPlayer.js
+++ scripts/radioPlayer.js
@@ -1,3 +1,69 @@
 export const radioPlayerInit = () => {
-  console.log('Radio Init');
+  const radio = document.querySelector('.radio');
+  const radioCoverImg = document.querySelector('.radio-cover__img');
+  const radioHeaderBig = document.querySelector('.radio-header__big');
+  const radioNavigation = document.querySelector('.radio-navigation');
+  const radioItem = document.querySelectorAll('.radio-item');
+  const radioStop = document.querySelector('.radio-stop');
+  const radioVolume = document.querySelector('.radio-volume');
+
+  const audio = new Audio();
+  audio.type = 'audio/aac';
+  // Блокируем кнопку воспроизведения
+  radioStop.disabled = true;
+
+  const changeIconPlay = () => {
+    if (audio.paused) {
+      radio.classList.remove('play');
+      radioStop.classList.add('fa-play');
+      radioStop.classList.remove('fa-stop')
+    } else {
+      radio.classList.add('play');
+      radioStop.classList.remove('fa-play');
+      radioStop.classList.add('fa-stop');
+    }
+  }
+
+  const selectItem = elem => {
+    radioItem.forEach((item) => item.classList.remove('select'));
+    elem.classList.add('select');
+  }
+
+  radioNavigation.addEventListener('change', event => {
+    const target = event.target;
+    const parent = target.closest('.radio-item');
+
+    selectItem(parent);
+
+    const title = parent.querySelector('.radio-name').textContent;
+    radioHeaderBig.textContent = title;
+
+    const urlImg = parent.querySelector('.radio-img').src;
+    radioCoverImg.src = urlImg
+
+    radioStop.disabled = false;
+    audio.src = target.dataset.radioStantion;
+    audio.play();
+    changeIconPlay();
+
+    //const radioBtn = target.closest('.radio-btn');
+    // radioVolume.addEventListener('input', () => {
+    // radioBtn.volume = radioVolume.value / 100;
+    // });
+    
+  });
+
+  radioStop.addEventListener('click', () => {
+    if (audio.paused) {
+      audio.play();
+    } else {
+      audio.pause()
+    };
+    changeIconPlay();
+  });
+
+  radioVolume.addEventListener('input', () => {
+    audio.volume = radioVolume.value / 100;
+  });
+    
 }
